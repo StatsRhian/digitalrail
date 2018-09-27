fdec5bc991dd5e45f5e7cb86bfc2aea77d4551f4
trianglegirl <thetrianglegirl@gmail.com>
2018-09-27 12:12:55 +0100

    Initial commit

---
+++ R/platform_get.R
@@ -0,0 +1,27 @@
+#' Get function
+#'
+#' This functions allows me to get a table from the digital rail platform, and decode the json format into a tibble.
+#' @param table_name The name of the table which you wawnt to get
+#' @keywords api
+#' @export
+#' @examples
+#' cat_function()
+#' 
+platform_get <- function(table_name){
+  get_url <- "http://digitalrail-platform:8091/datapoint/table/get"
+  r <- GET(get_url, query = list(table = table_name))
+  input_data <- content(r)$object$data
+  names_data <- content(r)$object$attributes
+  
+  df <- lapply(input_data, function(x){
+    x[sapply(x, is.null)] <- NA
+    unlist(x)
+  })
+  
+  df <- as.tibble(t(as.data.frame(df)))
+  names(df) <- names_data
+  return(df)
+} 
+  
+
+
