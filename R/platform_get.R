0e6f859d2bf14b69b3b3d54932e596a197dfc0a3
trianglegirl <thetrianglegirl@gmail.com>
2019-03-13 15:14:16 +0000

    Update manual and import packages

--- R/platform_get.R
+++ R/platform_get.R
@@ -4,24 +4,23 @@
 #' @param table_name The name of the table which you wawnt to get
 #' @keywords api
 #' @export
-#' @examples
-#' cat_function()
-#' 
+#'
+#'
 platform_get <- function(table_name){
   get_url <- "http://digitalrail-platform:8091/datapoint/table/get"
-  r <- GET(get_url, query = list(table = table_name))
+  r <- httr::GET(get_url, query = list(table = table_name))
   input_data <- content(r)$object$data
   names_data <- content(r)$object$attributes
-  
+
   df <- lapply(input_data, function(x){
     x[sapply(x, is.null)] <- NA
     unlist(x)
   })
-  
-  df <- as.tibble(t(as.data.frame(df)))
+
+  df <- tibble::as.tibble(t(as.data.frame(df)))
   names(df) <- names_data
   return(df)
-} 
-  
+}
+
 
 
--- R/platform_select.R
+++ R/platform_select.R
@@ -5,17 +5,16 @@
 #' @param previous_seconds The number of previous second to pull
 #' @keywords api
 #' @export
-#' @examples
-#' cat_function()
-#' 
+#'
+#'
 platform_select <- function(table_name, previous_seconds){
-  
+
   #Take current time in milliseconds
   current_time <- as.numeric(as.POSIXct(Sys.time())) * 1000
   start_time <- current_time - previous_seconds * 1000
-  
+
   select_url <- "http://digitalrail-platform:8091/datapoint/table/record/select"
-  
+
   attributes = list(list(name = "accelerometer_x", type = "float", as = ""),
                     list(name = "accelerometer_y", type = "float", as = ""),
                     list(name = "accelerometer_z", type = "float", as = ""),
@@ -31,20 +30,20 @@ platform_select <- function(table_name, previous_seconds){
     list(column = "time", operator = ">", value = round(start_time, 0), type = "timestamp")
   )
   request_body <- jsonlite::toJSON(list(table = table_name, attributes = attributes, clauses = clauses), pretty = TRUE, auto_unbox = TRUE)
-  
+
   # Send request
-  r <- POST(select_url, body = request_body, encode = "json")
-  
+  r <- httr::POST(select_url, body = request_body, encode = "json")
+
   # Unpack JSON response
   input_data <- content(r)$object$data
   names_data <- content(r)$object$attributes
-  
+
   df <- lapply(input_data, function(x){
     x[sapply(x, is.null)] <- NA
     unlist(x)
   })
-  
-  df <- as.tibble(t(as.data.frame(df)))
+
+  df <- tibble::as.tibble(t(as.data.frame(df)))
   names(df) <- names_data
   return(df)
-} 
+}
